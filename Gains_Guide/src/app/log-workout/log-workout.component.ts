0bff0a4582da7afd5cf2e6f0f9cec62c31b6d522
LinusLee2004 <linuslee2123@gmail.com>
2025-04-29 15:14:35 -0500

    Recreated components

---
+++ Gains_Guide/src/app/log-workout/log-workout.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-log-workout',
+  imports: [],
+  templateUrl: './log-workout.component.html',
+  styleUrl: './log-workout.component.css'
+})
+export class LogWorkoutComponent {
+
+}
---
+++ Gains_Guide/src/app/track-progress/track-progress.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-track-progress',
+  imports: [],
+  templateUrl: './track-progress.component.html',
+  styleUrl: './track-progress.component.css'
+})
+export class TrackProgressComponent {
+
+}
