8360563055dc77ea00c8b6936d55f81b49fdedbd
zqnd3r <brickinthewall9@gmail.com>
2025-05-08 13:35:27 -0500

    all done

--- Gains_Guide/src/firebaseConfig.ts
+++ Gains_Guide/src/firebaseConfig.ts
@@ -1,11 +1,7 @@
-// Import the functions you need from the SDKs you need
+
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 export const firebaseConfig = {
   apiKey: "AIzaSyCtoP1MG_5P9HIXdU4i7CV3EmtEIajAUK4",
   authDomain: "gainsguide-73109.firebaseapp.com",
@@ -16,6 +12,6 @@ export const firebaseConfig = {
   measurementId: "G-9WMYY5V4VX"
 };
 
-// Initialize Firebase
+
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
